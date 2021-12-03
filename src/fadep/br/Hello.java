5bf696b6cf43959ac9a2ef4034b1ec807316149f
WillianZatta <william_zatta@hotmail.com>
2021-12-03 09:26:28 -0300

    first commit

---
+++ src/fadep/br/Hello.java
@@ -0,0 +1,14 @@
+
+package fadep.br;
+
+/**
+ *
+ * @author willi
+ */
+public class Hello {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello,World");
+        
+    }
+}
